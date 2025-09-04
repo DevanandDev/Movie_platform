e514f187b5e02b0f398402137efb54771accfee9
Devanand <devanandk8078@gmail.com>
2025-09-04 12:15:44 +0530

    updated

--- lib/view/booking_page.dart
+++ lib/view/booking_page.dart
@@ -11,12 +11,31 @@ class MyBooking extends StatefulWidget {
 
 class _MyBookingState extends State<MyBooking> {
   
-late  TextEditingController title =TextEditingController();
+  TextEditingController title =TextEditingController();
+  TextEditingController lang =TextEditingController();
+  TextEditingController releas =TextEditingController();
+  TextEditingController media =TextEditingController();
+
+
 @override
   void initState() {
     // TODO: implement initState
     super.initState();
     title = TextEditingController(text: widget.movie.title);
+    lang = TextEditingController(text: widget.movie.originalLanguage);
+    releas = TextEditingController(text: widget.movie.releaseDate);
+    media = TextEditingController(text: widget.movie.voteCount.toString());
+
+  
+  }
+  void bookMovie() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Booking Successful!"),
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 2),
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -35,14 +54,55 @@ late  TextEditingController title =TextEditingController();
                 ),
               ),
             ),
-             Text('Movie',style: TextStyle(fontWeight: FontWeight.bold),),
+             Text('Language',style: TextStyle(fontWeight: FontWeight.bold),),
               TextField(
-              controller: title,
+              controller: lang,
               decoration: InputDecoration(
                 border: OutlineInputBorder(            
                 ),
               ),
             ),
+             Text('Released',style: TextStyle(fontWeight: FontWeight.bold),),
+              TextField(
+              controller: releas,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(            
+                ),
+              ),
+            ),
+             Text('Popularity',style: TextStyle(fontWeight: FontWeight.bold),),
+              TextField(
+              controller: media,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(            
+                ),
+              ),
+            ),SizedBox(height: 20,),
+             Row(mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 GestureDetector(
+                  onTap: () =>bookMovie() ,
+                   child: Container(
+                    width: 300,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(12), 
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Book Your Ticket',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                               ),
+                 ),
+               ],
+             ),
           ],
         ),
       ),
